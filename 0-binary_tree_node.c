641a9d3c0710f4ad7e189b319d800e73d3dafdf1
MoedCode <mohamed71291@gmail.com>
2023-11-28 18:06:23 +0200

    binary_trees Task0 (automated command)

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,14 +1,21 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_node - simply creat a new tree node then return it
+ *
+ * @parent: pointer to tree parent node
+ * @value: node data which is an integer value
+ * Return: (binary_tree_t*) newly created node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 
-    binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
-    new->parent = parent;
-    new->left = NULL;
-    new->right = NULL;
-    new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	new->n = value;
 
-    return (new);
+	return (new);
 
 }
