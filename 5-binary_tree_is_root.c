8780d8e94b49b31f0abd0b073355984456421b49
MoedCode <mohamed71291@gmail.com>
2023-11-29 01:09:31 +0200

    binary_trees Task5 (automated command)

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a given node is a root
+ *
+ * @node:checks if a given node is a root
+ * Return:(1) if concern node is root (0) otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node && node->parent == NULL)
+		return (1);
+	return (0);
+}
