bee49e17674104866063cb1646a5f8356dbe8771
MoedCode <mohamed71291@gmail.com>
2023-11-28 17:56:07 +0200

    moed commit (automated command)

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,2 +1,14 @@
-#include "main.h"
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {}
+#include "binary_trees.h"
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+    binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+    new->parent = parent;
+    new->left = NULL;
+    new->right = NULL;
+    new->n = value;
+
+    return (new);
+
+}
