9ed61ac56bad8699f0e6f64818d75a861a90d3d4
MoedCode <mohamed71291@gmail.com>
2023-11-29 00:15:25 +0200

    binary_trees Task4 (automated command)

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,13 +2,13 @@
 
 /**
  * binary_tree_is_leaf - hecks if a node is a leaf
- * @node: pointer to concerned node
+ * @node: pointer to concerned  node
  * Return: (1) if concerned node is a leaf , (0) otherwise
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (!node || node->left || node->right)
-        return (0);
-    return (1);
+	if (!node || node->left || node->right)
+		return (0);
+	return (1);
 }
 
