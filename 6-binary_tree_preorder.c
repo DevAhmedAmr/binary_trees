9cf2cb0c5046ff06a17b008ac0899e187150cf46
MoedCode <mohamed71291@gmail.com>
2023-11-29 03:46:30 +0200

    binary_trees Task6 (automated command)

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,12 +5,11 @@
  * @tree: pointer to root node of the concern tree
  * @func: function pointer to pe call with each node
  */
- void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
- {
-    if (!tree || !func)
-        return;
-    func(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
-
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
