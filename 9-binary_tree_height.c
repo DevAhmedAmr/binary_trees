cb9bd258801502fef8d346ebf9af9ca8f6bc297f
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-29 07:12:14 +0200

    9. Height (automated command)

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t num1 = 0, num2 = 0;
 	if (!tree)
-		return -1;
+		return (0);
 
-	size_t num1 = 1 + binary_tree_height(tree->left);
-	size_t num2 = 1 + binary_tree_height(tree->right);
+	if (tree->left)
+		num1 = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		num2 = 1 + binary_tree_height(tree->right);
 	return max(num1, num2);
 }
 size_t max(size_t num1, size_t num2)
