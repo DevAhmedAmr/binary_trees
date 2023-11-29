3f579e4abfa0e56ca163079c10ac7d003f084700
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-29 04:14:14 +0200

    4. Is leaf (automated command)

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,14 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - hecks if a node is a leaf
+ * binary_tree_is_leaf - checks if a node is a leaf
  * @node: pointer to concerned  node
  * Return: (1) if concerned node is a leaf , (0) otherwise
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node || node->left || node->right)
+	if (node->left || node->right)
 		return (0);
 	return (1);
 }
-
