ecffe1965ce3d76a63d8d2d91d4b0e8206c040a8
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-29 07:09:33 +0200

    9. Height (automated command)

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,13 +1,6 @@
 
 #include "binary_trees.h"
 
-size_t max(size_t num1, size_t num2)
-{
-	if (num1 > num2)
-		return num1;
-
-	return num2;
-}
 /**
  * binary_tree_height - goes through a binary tree using
  *post-order traversal
@@ -21,4 +14,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t num1 = 1 + binary_tree_height(tree->left);
 	size_t num2 = 1 + binary_tree_height(tree->right);
 	return max(num1, num2);
+}
+size_t max(size_t num1, size_t num2)
+{
+	if (num1 > num2)
+		return num1;
+
+	return (num2);
 }
