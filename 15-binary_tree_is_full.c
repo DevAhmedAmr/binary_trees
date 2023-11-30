158b1eb8efa5967f8bbc40285952aa6a7520d343
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-30 23:38:43 +0200

    15. Is full (automated command)

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,36 +1,22 @@
 #include "binary_trees.h"
-
-int recCheck(const binary_tree_t *tree);
 /**
- * binary_tree_is_full -  checks if a concern binary tree is full
+ * binary_tree_is_full - function that checks if a binary tree is full
  * @tree: pointer to root node of the concern tree
- * Return: number of nodes that has to child if it parent or no
- * child if it leaves ,(0) if tree pointer is NULL
+ * Return: 1 if full , 0 if not
+ * عظمة يا رايق:man_bowing:
  */
-
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int left, right;
+
 	if (!tree)
 		return (0);
-	return (recCheck(tree));
-}
 
-/**
- * recCheck - use recursion to check that all tree node is
- * full parent nodes or leaves .
- * @tree: A pointer to the root node of concern tree list.
- * Return: (1)If tree is full, (0).Otherwise .
- */
-int recCheck(const binary_tree_t *tree)
-{
-	if (!tree)
+	if (!tree->left && !tree->right)
 		return (1);
-	if ((!tree->right && tree->left) || (tree->right && !tree->left)
-		|| recCheck(tree->left) == 0  || recCheck(tree->right) == 0)
-		return (0);
-	return (1);
 
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+
+	return (left == right);
 }
-
-
-
