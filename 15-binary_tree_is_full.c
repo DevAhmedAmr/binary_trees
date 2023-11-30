0ae0b321a4216449bdfd6ace1c2e26abbd185788
MoedCode <mohamed71291@gmail.com>
2023-11-30 02:49:42 +0200

    binary_trees Task15 (automated command)

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_is_full -  checks if a concern binary tree is full
+ * @tree: pointer to root node of the concern tree
+ * Return: number of nodes that has to child if it parent or no
+ * child if it leaves ,(0) if tree pointer is NULL
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int is_full = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left && tree->right)
+	{
+		is_full = 1 + binary_tree_is_full(tree->left);
+		is_full = 1 + binary_tree_is_full(tree->right);
+	}
+	else
+	{
+		binary_tree_is_full(tree->left);
+		binary_tree_is_full(tree->right);
+	}
+	return (is_full);
+
+}
