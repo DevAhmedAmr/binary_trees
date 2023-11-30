5bf362108916e82339b3e4b10c5c8f11d3024fd5
MoedCode <mohamed71291@gmail.com>
2023-12-01 00:18:22 +0200

    binary_trees Task16 (automated command)

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -110,6 +110,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree))
 		return (1);
 	return (0);
