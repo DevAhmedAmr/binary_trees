adc2c4b83b1c53b345505ca9f30f8a16fcea8985
MoedCode <mohamed71291@gmail.com>
2023-11-30 22:50:50 +0200

    binary_trees Task18 (automated command)

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,22 +1,43 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to concern node
+ * Return: (binary_tree_t * node)  sibling of concern node ,
+ * (0) if not, or parent or concern node is null
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->left == node)
+	{
+		if (node->parent->right)
+			return (node->parent->right);
+	}
+	else if (node->parent->right == node)
+	{
+		if (node->parent->left)
+			return (node->parent->left);
+	}
+
+	return (NULL);
+}
+
+
 /**
  * binary_tree_uncle - finds the uncle of a node
  * @node: pointer to concern node
  * Return: (binary_tree_t * node)  sibling of concern node ,
  * (0) if not, or parent or concern node is null
  */
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 
 	if (!node)
 		return (0);
-	return (binary_tree_sibling(node));
+	return (binary_tree_sibling(node->parent));
 }
 
-/*
-					     [*B|root A |*C]
-
-			[*D |B| *C]   				          [*E|C|*F]
-
-[NULL |D| NULL]   [NULL|C|NULL]			[NULL|E| NULL]   [NULL|F|NULL]
-*/
