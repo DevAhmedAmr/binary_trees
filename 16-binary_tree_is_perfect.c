07df6d9bd13e78fb49603053eefe2350567b3b4c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-01 13:35:04 +0200

    task 16 hotfix (automated command)

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,14 +15,15 @@ int get_heights(const binary_tree_t *tree)
 	return 1 + (left_height > right_height ? left_height : right_height);
 }
 
-int binary_tree_is_perfect(const binary_tree_t *tree)
+int binary_tree_is_perfect_jr(const binary_tree_t *tree, int level)
 {
 	int left_height, right_height;
 
+	if (level == 0 && (tree == NULL))
+		return 0;
+
 	if (tree == NULL)
-	{
 		return 1;
-	}
 
 	left_height = get_heights(tree->left);
 	right_height = get_heights(tree->right);
@@ -32,5 +33,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return 0;
 	}
 
-	return binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right);
+	return binary_tree_is_perfect_jr(tree->left, level + 1) &&
+		   binary_tree_is_perfect_jr(tree->right, level + 1);
+}
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	return binary_tree_is_perfect_jr(tree, 0);
 }
