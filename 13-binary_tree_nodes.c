a43b1d413f36d6b7de6edfc86279bdc874391c29
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-01 04:07:11 +0200

    13. Nodes hot fix  (automated command)

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,30 +1,44 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - counts the leaves in a binary tree
- *post-order traversal
+ * binary_tree_nodes - function that counts the nodes with at least
+ *  1 child in a binary tree
  * @tree: pointer to root node of the concern tree
- * Return: the size of a binary tree
- * عظمة يا رايق:man_bowing:
+ *
+ * Return: If tree is NULL, the function must return 0
+ * A NULL pointer is not a node
  */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t leavCount = 0;
+    size_t leaves = 0;
 
     if (!tree)
         return (0);
+    if (tree->left || tree->right)
+        leaves++;
 
-
-    if (!tree->left && !tree->right)
-    {
-        leavCount++;
-        leavCount += binary_tree_leaves(tree->left);
-        leavCount += binary_tree_leaves(tree->right);
-    }
-    else
-    {
-        leavCount += binary_tree_leaves(tree->left);
-        leavCount += binary_tree_leaves(tree->right);
-    }
-    return (leavCount);
+    leaves += binary_tree_nodes(tree->left);
+    leaves += binary_tree_nodes(tree->right);
+    return (leaves);
 }
+
+/*size_t binary_tree_leaves(const binary_tree_t *tree)*/
+/*{*/
+/*size_t leavCount = 0;*/
+
+/*if (!tree)*/
+/*  return (0);*/
+
+/*if (!tree->left && !tree->right)*/
+/*{*/
+/*  leavCount++;*/
+/*leavCount += binary_tree_leaves(tree->left);*/
+/*leavCount += binary_tree_leaves(tree->right);*/
+/* }*/
+/*else*/
+/*{*/
+/*  leavCount += binary_tree_leaves(tree->left);*/
+/*leavCount += binary_tree_leaves(tree->right);*/
+/*}*/
+/*return (leavCount);*/
+/*}*/
